47d93e63aaedbb2f94b4a3d84ba7902f224e47be
tjqlqkqh12 <67355208+tjqlqkqh12@users.noreply.github.com>
2020-08-26 22:45:33 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	//풀페이지
+	$("#fullpage").fullpage({
+		navigation: true,
+		navigationTooltips:['TOP','PORTFOLIO-1','PORTFOLIO-2','PORTFOLIO-3','PORTFOLIO-4','ABOUT ME'],
+        anchors: ['firstPage','secondPage','thirdPage','fourthPage','fifthPage','lastPage'],
+        
+	});
+    
+})
